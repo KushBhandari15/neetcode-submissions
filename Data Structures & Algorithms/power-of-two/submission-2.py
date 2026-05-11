3718afb46c9a04584b4454e6c265c344775641cb
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2026-05-11 08:44:52 -0400

    Add: power-of-two - submission-2

---
+++ Data Structures & Algorithms/power-of-two/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+
+        def helper(n):
+
+            if n == 1:
+                return True
+            if  n == 0 or n % 2 != 0:
+                return False
+            
+            return helper(n/2)
+        
+        return helper(n)
+
