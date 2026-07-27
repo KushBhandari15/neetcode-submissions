2e319d02413d3cf8effa3dd4c845817bf3cc529c
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2026-07-27 12:12:56 +0200

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        if not s:
+            return True
+        pointer = 0
+        n = len(s)
+        for char in t:
+            if s[pointer] == char:
+                pointer += 1
+            if pointer >= n:
+                return True
+        
+        return False
