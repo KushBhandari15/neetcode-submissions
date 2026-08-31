4a5c23d3cdcd7a24464b1e48b8ac432806d8e671
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2026-08-31 16:45:12 +0200

    Add: simplify-path - submission-2

---
+++ Data Structures & Algorithms/simplify-path/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        stack = []
+        i = 0
+        n = len(path)
+        while i < n:
+            while i < n and path[i] == "/":
+                i += 1
+
+            directory = ""
+            while i < n and path[i] != "/":
+                directory += path[i]
+                i += 1
+            
+            if directory == "..":
+                if stack:
+                    stack.pop()
+            elif directory == ".":
+                pass
+            else:
+                if directory:
+                    stack.append(directory)
+            
+        path = '/'.join(stack)
+        path = '/' + path
+
+        return path
